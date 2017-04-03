bf21565fdf647aee196f3907f8452952693e69a2
Juliana Furgala <Juliana.Furgala@tufts.edu>
2017-04-03 01:27:33 -0400

    add basic keypress logic to update middle of screen during play

--- assets/js/roundlogic.js
+++ assets/js/roundlogic.js
@@ -3,31 +3,34 @@
 /* uses listeners to update user typed data for all 4 users and presents it on screen */
 /* updates sentence in the middle on user #'s screen when that user has entered the last line correctly */
 
-var full_story = "Once there was a little boy named Jack. He was sent to the market to sell a cow for money for food. However he chose to sell the cow for a magical seed that grew a beanstack to a giant's castle! What did Jack do next?" // for test purposes only
+var full_story = "Once there was a little boy named Jack. He was sent to the market to sell a cow for money for food. However he chose to sell the cow for a magical seed that grew a beanstack to a giant's castle! What did Jack do next? He climbed the beanstack of course!" // for test purposes only
 
 var story;
 var line_num = 1;
 
-parse_story()
+// into paragraphs and then sentences
+function parse_story()
 {
     // http://www.jquerywithexample.com/2015/11/split-string-with-multiple-separator-in.html
-    story = full_story.split(/.|?|!/);
+    story = full_story.split('.'); /* /.|?|!/); */
 
+    // console.log(story);  // REMOVE THIS LATER
     line = document.getElementById("center");
     line.innerHTML += story[0];
 }
 
-find_wpm(user_time)
+function find_wpm(user_time)
 {
     var word_count = story.split(" ");
-    return word_count.length() / user_time;
+    word_count.length / user_time;
 }
 
-check_input()
+function check_input()
 {
     line = document.getElementById("center");
-    
-    if (line_num > story.length()) {
+
+    console.log(story);
+    if (line_num > story.length) {
         line.innerHTML = "You finished this round!"; 
     }	
 	
@@ -35,13 +38,14 @@ check_input()
     
     if (input === story[line_num - 1]) {
 	line_num += 1;
-        line.innerHTML = story[line_num - 1]; 
+        line.innerHTML = story[line_num - 1];
+	// console.log("Hello");
         // line = story[line_num];
     }
 }
 
 // .keypress
-update_textboxes()
+function update_textboxes()
 {
     newMsg = document.getElementById("hbox_top").value;
     // TBAdded
