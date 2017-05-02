0e08c4a9782260861dd788cd33a55b06be741ba3
Bianca Capretta <bianca.capretta@gmail.com>
2017-05-02 12:25:50 -0400

    Switched to innerHTML

--- assets/js/memelogic.js
+++ assets/js/memelogic.js
@@ -33,8 +33,8 @@ function memelordStatus() {
 // need to add server time
 function sendMeme (randomNum) {
     var img = $('<img>', {id: "image", src: "../img/meme"+randomNum+".jpg", alt: "memeattack!"});
-    $("center").html(img);
+    $("center").innerHTML(img);
     setTimeout(function () {
-        $("center").html($center);
+        $("center").innerHTML($center);
     }, 3000);
 }
